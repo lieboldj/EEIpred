cd8d267bc44cb3d4deb1cc3a4f59586f3d5b1157
Liebold, Jeanine <jeanine.liebold@uni-hamburg.de>
2025-05-26 16:38:12 +0200

    simplify prot to exon for benchmarking

--- glinter/scripts/run_prot_to_exon.sh
+++ glinter/scripts/run_prot_to_exon.sh
@@ -1,14 +1,17 @@
-CUDA_VISIBLE_DEVICES=1 python glinter/models/msa_model_train1.py --dimer-root x --feature heavy-atom-graph,surface-graph,coordinate-ca-graph,pickled-esm --fold 1 --data_root ../data_collection/cv_splits/ --dataset RUNTIME
-python scripts/from_prot_to_exons.py --fold 1 --dataset RUNTIME --mode test
-#python scripts/from_prot_to_exons.py --fold 1 --dataset CONTACT --mode train
-#python scripts/from_prot_to_exons.py --fold 2 --dataset CONTACT --mode train
-#python scripts/from_prot_to_exons.py --fold 3 --dataset CONTACT --mode train
-#python scripts/from_prot_to_exons.py --fold 4 --dataset CONTACT --mode train
-#python scripts/from_prot_to_exons.py --fold 5 --dataset CONTACT --mode train
-#
-#python scripts/from_prot_to_exons.py --fold 1 --dataset CONTACT --mode test
-#python scripts/from_prot_to_exons.py --fold 2 --dataset CONTACT --mode test
-#python scripts/from_prot_to_exons.py --fold 3 --dataset CONTACT --mode test
-#python scripts/from_prot_to_exons.py --fold 4 --dataset CONTACT --mode test
-#python scripts/from_prot_to_exons.py --fold 5 --dataset CONTACT --mode test
+#CUDA_VISIBLE_DEVICES=1 python glinter/models/msa_model_train1.py --dimer-root x --feature heavy-atom-graph,surface-graph,coordinate-ca-graph,pickled-esm --fold 1 --data_root ../data_collection/cv_splits/ --dataset RUNTIME
+#python scripts/from_prot_to_exons.py --fold 1 --dataset RUNTIME --mode test
 
+datasets=(CLUST_CONTACT CLUST_EPPIC CLUST_PISA)
+modes=(train test val)
+folds=(1 2 3 4 5)
+
+for dataset in "${datasets[@]}"; do
+    for mode in "${modes[@]}"; do
+        for fold in "${folds[@]}"; do
+            echo "Running from_prot_to_exons.py with Dataset: $dataset, Mode: $mode, Fold: $fold"
+            python scripts/from_prot_to_exons.py --fold "$fold" --dataset "$dataset" --mode "$mode"
+        done
+    done
+done
+
+#python scripts/from_prot_to_exons.py --fold 1 --dataset CLUST_CONTACT --mode train
